e3d36b3005f21dcc6c5da1c128676a021aa25a13
Wendel Lopes <wendelopes@msn.com>
2020-06-11 16:49:17 -0300

    Configure custom.js

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,13 +1,9 @@
-$(document).ready(function(){
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-  });
-
-  $(".delete").on('click', function(){
-    $(this).parent().remove();
-  });
-
+// Check for click events on the navbar burger icon
+$(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+});
+// Close notification
+$(".delete").on('click', function(){
+  $(this).parent().remove();
 });
