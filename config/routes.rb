d73e7a6743cb14e9c1b27735baefb9bf6296d265
Wendel Lopes <wendelopes@msn.com>
2020-06-22 14:19:40 -0300

    Customized portifolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   resources :blogs
-  resources :portifolios
+  resources :portifolios, except: [:show]
+  get 'portifolio/:id', to: 'portifolios#show', as: 'portifolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
